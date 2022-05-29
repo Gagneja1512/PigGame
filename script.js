0e745d23df6527d92d62a49896760169866d1944
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-29 14:07:38 +0530

    Switching the active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0') ;
+const player1El = document.querySelector('.player--1') ;
 const score0El = document.querySelector('#score--0') ;
 const score1El = document.getElementById('score--1') ;
 const diceEl = document.querySelector('.dice') ;
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden') ;
 
+const scores = [0 , 0] ;
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click' , function(){
@@ -28,11 +32,18 @@ btnRoll.addEventListener('click' , function(){
     if(dice !== 1)
     {
         currentScore = currentScore + dice ;   
-        current0El.textContent = currentScore ;  
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore ;
     }
     else 
-    {
+    {   
+        document.getElementById(`current--${activePlayer}`).textContent = 0 ;
 
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0 ; 
+
+        player0El.classList.toggle('player--active') ;
+        player1El.classList.toggle('player--active') ;
+        
     }
 
-})
+});
